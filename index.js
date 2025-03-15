4602497b013bb034c23edc3d3f970af902a2759a
Raudy Garcia Castro <134640697+RaudyCode@users.noreply.github.com>
2025-03-15 15:36:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use('/api', apiRoutes)
 
 
 // definir un puerto y arrancar el proyecto
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, ()=>{
     console.log(`El servidor esta funcionando en el puerto ${port}`)
 })
